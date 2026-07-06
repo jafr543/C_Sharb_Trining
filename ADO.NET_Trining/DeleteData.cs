d2983cadbc827f51127966566391668c242a7a30
jafr jaber <jafrsyd543@gmail.com>
2026-07-06 13:14:06 +0300

    Add DeleteData class to remove contacts from database
    Implement a method to delete a contact from the database using ContactID.

---
+++ ADO.NET_Trining/DeleteData.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Data.SqlClient;
+using System.Net;
+using System.Security.Policy;
+using static Program;
+internal class Program
+{
+
+    static string connectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=123456;";
+  
+    static void AddNewRow(int ContactID)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = @"Delete Contacts
+                            Where ContactID = @ContactID";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@ContactID", ContactID);
+
+
+        try
+        {
+            connection.Open();
+
+            int result = command.ExecuteNonQuery();
+
+            if (result > 0)
+            {
+                Console.WriteLine($"Record Deleted Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Record Delete failed");
+            }
+
+            
+            connection.Close();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        
+    } 
+
+    static void Main(string[] args)
+    { 
+        AddNewRow(9);
+        Console.ReadKey();
+    }
+}
