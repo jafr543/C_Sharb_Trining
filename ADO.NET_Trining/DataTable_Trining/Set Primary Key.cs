22c9dc8283cf8cfa00ceace493d4d0438728dd88
jafr jaber <jafrsyd543@gmail.com>
2026-07-11 16:21:01 +0300

    Create DataTable with 'ID' as Primary Key
    This code initializes a DataTable for employees, sets the 'ID' column as the primary key, and populates it with sample data.

---
+++ ADO.NET_Trining/DataTable_Trining/Set Primary Key.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Data;
+using System.Linq;
+
+namespace Trining
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            DataTable Employees = new DataTable();
+
+            Employees.Columns.Add("ID", typeof(int));
+            Employees.Columns.Add("Name", typeof(string));
+            Employees.Columns.Add("Country", typeof(string));
+            Employees.Columns.Add("Salary", typeof(Double));
+            Employees.Columns.Add("Date", typeof(DateTime));
+
+
+
+            DataColumn[] PrimaryKeysColumns = new DataColumn[1];
+            PrimaryKeysColumns[0] = Employees.Columns["ID"];
+            Employees.PrimaryKey = PrimaryKeysColumns;
+
+
+
+            Employees.Rows.Add(1, "Jafr Jaber", "Iraq", 600000, DateTime.Now);
+            Employees.Rows.Add(2, "Noor Jaber", "Iraq", 1000000, DateTime.Now);
+            Employees.Rows.Add(3, "Hider Sadee", "Canada", 150000, DateTime.Now);
+            Employees.Rows.Add(4, "Ahmed Safi", "China", 0.00, DateTime.Now);
+            Employees.Rows.Add(5, "Fadi Jameel", "Egypt", 800, DateTime.Now);
+
+
+            
+            Console.WriteLine("Employees List with ID as Primary Key");
+            foreach (DataRow row in Employees.Rows)
+            {
+                Console.WriteLine("ID: {0} \tName: {1} \tCountry: {2} \t Salary: {3} \t Date: {4}",
+                    row["ID"], row["Name"], row["Country"], row["Salary"], row["Date"]);
+            }
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
