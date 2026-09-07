ee168e47e830ed2501ee7b2e38cabb235ea0cb5d
jafr jaber <jafrsyd543@gmail.com>
2026-09-07 14:22:25 +0300

    Implement basic operations with SortedList in C#
    Demonstrates adding, accessing, iterating, and removing elements in a SortedList.

---
+++ Data Structures in C#/SortedList/Working with SortedList.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var sortedList = new SortedList<string, int>();
+
+
+        // Adding elements
+
+        sortedList.Add("banana", 2);
+        sortedList.Add("Orange", 3);
+        sortedList.Add("apple", 3);
+
+        // Accessing elements
+        Console.WriteLine("\nQuantity of apples: " + sortedList["apple"]);
+
+
+        Console.WriteLine("\nIterating SortedList Elements:");
+        // Iterating through elements
+        foreach (var item in sortedList)
+        {
+            Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+        }
+
+
+        // Removing an element
+        sortedList.Remove("apple");
+
+        Console.WriteLine("\nSortedList Elements removing apple:");
+
+        foreach (var item in sortedList)
+        {
+            Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+        }
+
+        // Waiting for a key press
+        Console.ReadKey();
+    }
+}
