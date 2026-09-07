325ae746dae690493a3ea566068e62e69e1cfb58
jafr jaber <jafrsyd543@gmail.com>
2026-09-07 15:02:32 +0300

    Add LINQ examples with SortedSet in C#
    Demonstrates LINQ operations on a SortedSet including filtering, summing, and sorting.

---
+++ Data Structures in C#/SortedSet/Linq with SortedSet.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+       
+            SortedSet<int> sortedSet = new SortedSet<int>() { 1, 2, 3, 4, 5 };
+
+
+            // Filtering elements greater than 2
+            var filteredSet = sortedSet.Where(x => x > 2);
+            Console.WriteLine("Filtered Set:");
+            foreach (var item in filteredSet)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            // Sum of all elements
+            var sum = sortedSet.Sum();
+            Console.WriteLine("Sum of all elements: " + sum);
+
+
+            // Maximum and minimum elements
+            var maxElement = sortedSet.Max();
+            var minElement = sortedSet.Min();
+            Console.WriteLine("Maximum element: " + maxElement);
+            Console.WriteLine("Minimum element: " + minElement);
+
+
+            // Sorting the set in descending order
+            var descendingSet = sortedSet.OrderByDescending(x => x);
+            Console.WriteLine("Descending Sorted Set:");
+            foreach (var item in descendingSet)
+            {
+                Console.Write(item + " ");
+            }
+
+
+
+
+        SortedSet<int> numbers = new SortedSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Find even numbers and project them to their square
+        var evenNumbersSquared = numbers.Where(x => x % 2 == 0).Select(x => x * x);
+        Console.WriteLine("Squares of even numbers:");
+        foreach (var item in evenNumbersSquared)
+        {
+            Console.Write(item + " ");
+        }
+
+        // Waiting for a key press
+        Console.ReadKey();
+        
+
+
+    }
+}
